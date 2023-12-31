fe23bdf5cc0157c7d04d51da6d1ac95c16c14842
Anubhav Singh <Anubhav.singh@worka.me>
2023-12-31 22:57:45 +0530

    Added few more section

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -2,80 +2,22 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
-        <section>
-          <section class="sticky">
-            <div class="max-w-lg px-4 sm:pt-24 pt-12 sm:pb-8 mx-auto text-left md:max-w-none md:text-center">
-
-              <h1 class="font-extrabold leading-10 tracking-tight text-left text-[#201515] text-center sm:leading-none text-5xl sm:text-9xl">
-                <span class="inline md:block">Building Good </span>
-                <span class="relative mt-2 bg-clip-text text-[#201515] md:inline-block">
-                  Software.
-                </span>
-              </h1>
-            </div>
-
-            <div class="max-w-lg px-4 pb-24 mx-auto text-left md:max-w-none md:text-center">
-              <div class="text-center py-4 space-x-4">
-                <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-[#FF4F01] border border-[#E2E8F0] translate-y-1 text-white hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
-                  <span> Build Your Custom App</span>
-                </button>
-
-                <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-white border border-[#E2E8F0] translate-y-1 text-[#16161d] hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
-                  <span> Explore Apps</span>
-                </button>
-              </div>
-            </div>
-          </section>
-        </section>
-
-        <div class="text-left">
-          <div class="sm:px-28">
-            <section class="relative flex items-center w-full">
-              <div class="relative items-center w-full px-5 mx-auto md:px-12 lg:px-16 max-w-7xl">
-                <div class="relative flex-col items-start m-auto align-middle">
-                  <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
-                    <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
-                      <div class="max-w-xl text-center lg:text-left">
-                        <div>
-                          <p class="text-3xl font-semibold tracking-tight text-[#201515] sm:text-5xl">
-                            Space Management Software
-                          </p>
-                          <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
-                            Use this paragraph to share information about your
-                            company or products. Make it engaging and
-                            interesting, and showcase your brand's personality.
-                            Thanks for visiting our website!
-                          </p>
-                        </div>
-                        <div class="flex justify-center gap-3 mt-10 lg:justify-start">
-                          <a
-                            class="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600"
-                            href="#"
-                          >
-                            <span> Read more &nbsp; → </span>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
-                      <img
-                        class="object-cover rounded-3xl object-center w-full mx-auto bg-gray-300 lg:ml-auto "
-                        alt="hero"
-                        src="https://i.pinimg.com/originals/2e/2b/21/2e2b21aeed393403d4620367f9e093f9.gif"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-          </div>
-
-          <div class="mt-32" />
-
-          <section></section>
-        </div>
+    <div className="relative font-sans">
+      <img
+        src="https://assets.bonkerscorner.com/uploads/2023/07/23231909/main_banner_web.jpg"
+        alt="Banner Image"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="min-h-[400px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+        <h2 className="sm:text-5xl text-3xl font-bold mb-6">
+          Explore the Latest Collection
+        </h2>
+        <a
+          href="javascript:void(0)"
+          className="mt-8 bg-transparent text-white text-base font-semibold py-2.5 px-6 border-2 border-white rounded hover:bg-white hover:text-black transition duration-300 ease-in-out"
+        >
+          Latest Collection
+        </a>
       </div>
     </div>
   );
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,15 +6,23 @@ import { Route, RouterProvider , createBrowserRouter, createRoutesFromElements }
 import Layout from "./Components/Layout.jsx"
 import ShopCategory from "./Pages/ShopCategory.jsx"
 import Shop from './Pages/Shop.jsx'
+import Product from './Pages/Product.jsx'
+import Cart from './Pages/Cart.jsx'
+import LoginSignup from './Pages/LoginSignup.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
        <Route path="" element={<Shop />} />
-      <Route path='/men' element={<ShopCategory/>}></Route>
-      <Route path='/women' element={<ShopCategory/>}></Route>
-      <Route path='/accessories' element={<ShopCategory/>}></Route>
+      <Route path='/men' element={<ShopCategory category="men"/>}></Route>
+      <Route path='/women' element={<ShopCategory category="women"/>}></Route>
+      <Route path='/accessories' element={<ShopCategory category="accessories"/>}></Route>
+      <Route path='/product' element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+      <Route path='/login' element={<LoginSignup/>}></Route>
     </Route>
   )
 )
